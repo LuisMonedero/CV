686d9dad057d66d1c949fda658b8d46fab6d3282
Luis <ercrystal966@gmail.com>
2021-02-26 21:12:32 +0100

    heroku2

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/my-cv'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/my-cv/index.html'));});
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 app.listen(process.env.PORT || 8080);
